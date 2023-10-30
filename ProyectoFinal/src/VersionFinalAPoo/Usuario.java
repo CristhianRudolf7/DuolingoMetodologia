c0a715a02db99cf598c94bf41dadbc327001cca1
SilencedSign87 <diazvegabruno@hotmail.com>
2023-10-30 18:35:54 -0500

    cambios a la clase Usuario > versión final POO

--- ProyectoFinal/src/VersionFinalAPoo/Usuario.java
+++ ProyectoFinal/src/VersionFinalAPoo/Usuario.java
@@ -2,6 +2,8 @@ package VersionFinalAPoo;
 
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 public class Usuario {
     //atributos
     private String token;
@@ -10,7 +12,9 @@ public class Usuario {
     public Usuario(){
         this.token=entradaStg();
     }
-    private String entradaStg(){
+    private String entradaStg(String mensaje, String titulo){
+        String aux="";
+        aux=JOptionPane.showInputDialog(null,"ingrese"+mensaje,titulo,JOptionPane.INFORMATION_MESSAGE);
         
     }
 }
