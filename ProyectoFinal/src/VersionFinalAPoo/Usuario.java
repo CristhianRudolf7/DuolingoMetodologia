bf1b7e300e3dc20161c21efa381af10500b79fed
SilencedSign87 <diazvegabruno@hotmail.com>
2023-10-30 18:45:18 -0500

    archivos eliminados y entradas en la clase usuario>versión final

--- ProyectoFinal/src/VersionFinalAPoo/Usuario.java
+++ ProyectoFinal/src/VersionFinalAPoo/Usuario.java
@@ -7,19 +7,30 @@ import javax.swing.JOptionPane;
 public class Usuario {
     //atributos
     private String token;
-    private ArrayList<>
+    
     //constructor
     public Usuario(){
-        this.token=creaToken();
+        creaToken();
     }
     private void creaToken(){
-
+        String nombre =entradaStg("su nombre","Creando token");
+        String apellido=entradaStg("su apellido", "Creando token");
+        int nombrelong=nombre.length();
+        int apellidolong=apellido.length();
+        int index=0;
+        for (int i = 0; i < 2; i++) {
+            index=(int)Math.floor(Math.random()*(nombrelong));
+            this.token=token+nombre.charAt(index);
+        }
+        for (int i = 0; i < 2; i++) {
+            index=(int)Math.floor(Math.random()*(apellidolong));
+            this.token=token+apellido.charAt(index);
+        }
     }
-    private String entradaStg() {
+    private String entradaStg(String mensaje,String titulo) {
         String cadena = null;
         while (cadena == null || cadena.isEmpty()) {
-            cadena = JOptionPane.showInputDialog(null, "Ingrese el nombre del candidato:", "Registro del candidato",
-                    JOptionPane.INFORMATION_MESSAGE);
+            cadena = JOptionPane.showInputDialog(null, "Ingrese "+mensaje,titulo,JOptionPane.INFORMATION_MESSAGE);
             System.out.println(cadena);
             if (cadena == null) { // para que cancele la creación de un candidato
                 return "-1";
